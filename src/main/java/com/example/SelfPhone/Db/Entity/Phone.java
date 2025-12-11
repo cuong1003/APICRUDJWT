dc3fe46762851d405afc228eea61caf25fca3365
alone nugget <fakeymeo@gmail.com>
2025-12-11 14:00:19 +0700

    Add Redis Cache Phone query

--- src/main/java/com/example/SelfPhone/Db/Entity/Phone.java
+++ src/main/java/com/example/SelfPhone/Db/Entity/Phone.java
@@ -6,13 +6,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "Phone")
-public class Phone {
+public class Phone implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
--- src/main/java/com/example/SelfPhone/Service/PhoneService.java
+++ src/main/java/com/example/SelfPhone/Service/PhoneService.java
@@ -3,6 +3,9 @@ package com.example.SelfPhone.Service;
 import com.example.SelfPhone.Db.Entity.Phone;
 import com.example.SelfPhone.Db.Repository.PhoneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,22 +15,25 @@ public class PhoneService {
     @Autowired
     PhoneRepository phoneRepository;
 
+    @Cacheable(value="phones", key="'all'")
     public List<Phone> getAllPhone() {
         return phoneRepository.findAll();
     }
 
+    @Cacheable(value="phones", key="#id")
     public Phone findPhoneById(Integer id) {
         return phoneRepository.findPhoneById(id);
     }
-
+    @CachePut(value="phones", key="#id")
     public void savePhone(Phone phone) {
         phoneRepository.save(phone);
     }
-
+    @CachePut(value="phones", key="#id")
     public void updatePhone(Phone phone) {
         phoneRepository.save(phone);
     }
 
+    @CacheEvict(value="phones", key="#id")
     public void deletePhone(Integer id) {
         phoneRepository.delete(findPhoneById(id));
     }
